ae87f8b10c6f8aa40ec3bc89d176c922fe12dfd4
Benoît Suttor <bsuttor@cirb.irisnet.be>
2011-10-12 09:28:56 +0200

    clean content types

--- cirb/novac/interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- extra stuff goes here -*-
-from novacitoyen import INovaCitoyen
-from privatefolder import IPrivateFolder
-from listprivatefolder import IListPrivateFolder
-from publicfolder import IPublicFolder
--- cirb/novac/interfaces/novacitoyen.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-# -*- Additional Imports Here -*-
-
-
-class INovaCitoyen(Interface):
-    """A plone plugin for Nova Citoyen"""
-
-    # -*- schema definition goes here -*-
