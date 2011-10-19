c9b6186d642afe2daa949262d0c5e5aa2274886c
Benoît Suttor <bsuttor@cirb.irisnet.be>
2011-10-19 17:11:22 +0200

    add map in public page

--- cirb/novac/browser/publicview.py
+++ cirb/novac/browser/publicview.py
@@ -38,6 +38,7 @@ class PublicView(BrowserView):
         self.request = request
         registry = getUtility(IRegistry)
         self.novac_url = registry['cirb.novac.novac_url']
+        self.urbis_url = registry['cirb.urbis.urbis_url']
         
     @property
     def portal_catalog(self):
@@ -61,7 +62,7 @@ class PublicView(BrowserView):
             error=True
             msg_error=_(u'No folder id')
         
-        return {'novac_url':self.novac_url,'folder_id':folder_id,'error':error,'msg_error':msg_error}
+        return {'novac_url':self.novac_url,'urbis_url':self.urbis_url ,'folder_id':folder_id,'error':error,'msg_error':msg_error}
         
     
     def python_json(self):
@@ -93,6 +94,9 @@ class PublicView(BrowserView):
             return {'data':data, 'error':error, 'msg_error':msg_error, 'called_url':url}
         finally:
             socket.setdefaulttimeout(oldtimeout)
+        
+        msgid = _(u"not_available")
+        not_avaiable = self.context.translate(msgid)
         import json
         data = json.loads(data_from_url)
         properties = data['properties']
@@ -102,24 +106,38 @@ class PublicView(BrowserView):
                                     properties['zipCode'],
                                     properties['municipality'],)
         except:
-            error=True
-            msg_error += 'Error with address<br />'            
+            address = not_avaiable         
         try:
             type_dossier = properties['typeDossier']
         except:
-            error=True
-            msg_error += 'Error with typeDossier<br />'
+            type_dossier = not_avaiable
         try:
             desc = properties['object']
         except:
-            error=True
-            msg_error += 'Error with object (desc)<br />'
+            desc = not_avaiable
         try:
             ref = properties['novaRef']
         except:
-            ref=''
-            error=True
-            msg_error += 'Error with novaRe<br />'
-        results={'address':address, 'type_dossier':type_dossier,'desc':desc,'ref':ref, 'num_dossier':num_dossier}
+            ref  = not_avaiable
+        try:
+            folder_filed = properties['folderFiled']
+        except:
+            folder_filed  = not_avaiable
+        try:
+            introduce_on = properties['introduceOn']
+        except:
+            introduce_on  = not_avaiable
+        try:
+            lang = properties['lang']
+        except:
+            lang  = not_avaiable
+        try:
+            status = properties['status']
+        except:
+            status  = not_avaiable
+            
+        results={'address':address, 'type_dossier':type_dossier,'desc':desc,'ref':ref, 
+                 'num_dossier':num_dossier, 'folder_filed':folder_filed, 'introduce_on':introduce_on,
+                 'lang':lang, 'status':status}
         
         return {'data':data, 'error':error, 'msg_error':msg_error, 'called_url':url, 'results':results}
--- cirb/novac/browser/scripts/novac_public.js
+++ cirb/novac/browser/scripts/novac_public.js
@@ -1,13 +1,72 @@
+OpenLayers.DOTS_PER_INCH = 90.71428571428572;
+OpenLayers.Util.onImageLoadErrorColor = 'transparent';
+var map, layer;
+var filter_geom;
+var markers_layer, features_layer;
 $(document).ready(function() {
-    var url_ws_waws = $('#ws_waws').html();
+    /*var url_ws_waws = $('#ws_waws').html();
     var folder_id = $('#folder_id').html();
     var url = "https://ws.irisnetlab.be/nova/pub/dossiers/";
     called_url = url+folder_id;
     
-    /*result= $.getJSON(  
+    result= $.getJSON(  
                 called_url, 
                 function(json) {  
                     var result = "Language code is  "+ json.responseData.language;  
                    alert(result);}) */
     //alert(called_url + " : "+result);
+    var url_ws_urbis = $('#ws_urbis').html();
+    
+    var mapOptions = { 
+        resolutions: [34.76915808105469, 17.384579040527345, 8.692289520263673, 4.346144760131836, 2.173072380065918, 1.086536190032959, 0.5432680950164795, 0.2716340475082398, 0.1358170237541199],
+        projection: new OpenLayers.Projection('EPSG:31370'),
+        maxExtent: new OpenLayers.Bounds(16478.795,19244.928,301307.738,304073.87100000004),
+        units: "meters", 
+        theme: "++resource++cirb.novac.images/",
+        controls: []
+    };
+    map = new OpenLayers.Map('map', mapOptions );
+    map.addControl(new OpenLayers.Control.PanZoomBar({
+            position: new OpenLayers.Pixel(2, 15)
+    }));
+    map.addControl(new OpenLayers.Control.Navigation());
+    
+    map.addControl(new OpenLayers.Control.Scale($('scale')));
+    map.addControl(new OpenLayers.Control.MousePosition({element: $('location')}));
+
+    var urbislayer = new OpenLayers.Layer.WMS(
+        "urbisFR",url_ws_urbis,
+        {layers: 'urbisFR', format: 'image/png' },
+        { tileSize: new OpenLayers.Size(256,256) }
+    );
+    map.addLayer(urbislayer);
+    
+    markers_layer = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers_layer);
+    
+    var style = new OpenLayers.Style({
+                    pointRadius: "${radius}",
+                    fillColor: "#ffcc66",
+                    fillOpacity: 0.8,
+                    strokeColor: "#cc6633",
+                    strokeWidth: "${width}",
+                    strokeOpacity: 0.8
+                }, {
+                    context: {
+                        width: function(feature) {
+                            return (feature.cluster) ? 2 : 1;
+                        },
+                        radius: function(feature) {
+                            var pix = 2;
+                            if(feature.cluster) {
+                                pix = Math.min(feature.attributes.count, 7) + 2;
+                            }
+                            return pix;
+                        }
+                    }
+                });
+    
+    map.setCenter(new OpenLayers.LonLat(150000.0, 170000.0));
+    
+    
 });
