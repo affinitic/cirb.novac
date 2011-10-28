3891a970287c5139d9ef7bbae468edccacfa5e81
Benoît Suttor <bsuttor@cirb.irisnet.be>
2011-10-28 09:09:13 +0200

    add comment request waws

--- cirb/novac/browser/novacview.py
+++ cirb/novac/browser/novacview.py
@@ -68,6 +68,31 @@ class NovacView(BrowserView):
 
 
 def called_url(request_url, request_headers): # request_headers is a dict
+    """
+    return from SSO (cas) : 
+    {'CONNECTION_TYPE': 'keep-alive', 
+    'HTTP_ACCEPT': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+    'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.202 Safari/535.1', 
+    'HTTP_REFERER': 'http://192.168.103.22:8080/Plone/novac', 
+    'SERVER_NAME': 'ubuntu', 
+    'GATEWAY_INTERFACE': 'CGI/1.1', 
+    'SERVER_SOFTWARE': 'Zope/(2.13.10, python 2.6.7, linux2) ZServer/1.1', 
+    'REMOTE_ADDR': '192.168.103.63', 
+    'HTTP_ACCEPT_LANGUAGE': 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4', 
+    'SCRIPT_NAME': '', 
+    'REQUEST_METHOD': 'GET', 
+    'HTTP_HOST': '192.168.103.22:8080', 
+    'PATH_INFO': '/Plone/novac/wawslistprivate_view', 
+    'SERVER_PORT': '8080', 
+    'SERVER_PROTOCOL': 'HTTP/1.1', 
+    'QUERY_STRING': 'ticket=ST-12-UtGcAWe0temAdFpymxaA-cas', 
+    'HTTP_ACCEPT_CHARSET': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 
+    'channel.creation_time': 1319631740, 
+    'HTTP_ACCEPT_ENCODING': 'gzip,deflate,sdch', 
+    'HTTP_COOKIE': '_ZopeId="47674318A5H8WR0M3jM"; 
+    __ac="Hd3DxhAw9EpdJ44gF5JC6EqNpL7S4KEP02KGBinNgGs0ZWE3ZmEyOUVuY3J5cHQgODQwNzI2MjU3OTch"', 
+    'PATH_TRANSLATED': '/Plone/novac/wawslistprivate_view'}
+    """
     oldtimeout = socket.getdefaulttimeout()
     results = ''
     msg_error=''
