76e5e7ee4c173484384b4f0b2d7a54c605d5f699
Benoît Suttor <bsuttor@cirb.irisnet.be>
2011-11-29 15:27:58 +0100

    add isOwner

--- cirb/novac/browser/privateview.py
+++ cirb/novac/browser/privateview.py
@@ -98,19 +98,23 @@ class PrivateView(BrowserView):
         results['specific_reference'] = get_properties(self.context, properties,'specificReference')
         results['x'] = get_properties(self.context, properties,'x')
         results['y'] = get_properties(self.context, properties,'y')
-     
+        
+        isOwner = get_properties(self.context, properties,'isOwner')
+        
         h_prop = json.loads(history)
         h_res = []
         for prop in h_prop:
-            consultationDate = get_properties(self.context, prop,"consultationDate")
+            from datetime import datetime
+            d = datetime.fromtimestamp(float(get_properties(self.context, prop,"consultationDate"))/1000)
+            consultationDate = d.strftime("%d/%m/%y %H:%M")
             keyName = get_properties(self.context, prop,"keyName")
             h_res.append({'consultationDate':consultationDate, 'keyName':keyName})
             
-        owner_folder = True
+        
         
         return {'novac_url':self.novac_url, 'urbis_url':self.urbis_url, 'error':error,'msg_error':msg_error,
                 'jsondata':jsondata, 'history':history, 'results':results,
-                'owner_folder':owner_folder,'h_res':h_res}
+                'isOwner':isOwner,'h_res':h_res}
     
     def activate_mandat(self):
         mandat = urllib.quote(self.request.form.get('mandat'))
