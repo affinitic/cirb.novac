6fe298a7becc5dc4148c6c86355b508c90870a51
kdeterme <kdeterme@cirb.irisnet.be>
2011-12-01 16:31:33 +0100

    set orthophotoplan on fiche detail

--- cirb/novac/browser/scripts/novac_public.js
+++ cirb/novac/browser/scripts/novac_public.js
@@ -16,32 +16,47 @@ $(document).ready(function() {
                    alert(result);}) */
     //alert(called_url + " : "+result);
     var url_ws_urbis = $('#ws_urbis').html();
-    var portal_url = $('#portal_url').html();
+    var url_ws_urbis_cache = $('#urbis_cache_url').html();
+    portal_url = $('#portal_url').html();
+    OpenLayers.ImgPath = portal_url+"/++resource++cirb.novac.images/";
     var x = $('#x').html();
     var y = $('#y').html();
     var mapOptions = {
-        resolutions: [34.76915808105469, 17.384579040527345, 8.692289520263673, 4.346144760131836, 2.173072380065918, 1.086536190032959, 0.5432680950164795, 0.2716340475082398, 0.1358170237541199],
+        resolutions: [1112.61305859375, 556.306529296875, 278.1532646484375, 139.07663232421876, 69.53831616210938, 34.76915808105469, 17.384579040527345, 8.692289520263673, 4.346144760131836, 2.173072380065918, 1.086536190032959, 0.5432680950164795, 0.2716340475082398],
         projection: new OpenLayers.Projection('EPSG:31370'),
         maxExtent: new OpenLayers.Bounds(16478.795,19244.928,301307.738,304073.87100000004),
         units: "meters", 
-        controls: []
+        controls: [],
+        theme: portal_url + "/++resource++cirb.novac.scripts/openlayers.css"
     };
-    OpenLayers.ImgPath = portal_url+"/++resource++cirb.novac.images/";
     map = new OpenLayers.Map('map', mapOptions );
-    map.addControl(new OpenLayers.Control.PanZoomBar({
-            position: new OpenLayers.Pixel(2, 15)
-    }));
+    
     map.addControl(new OpenLayers.Control.Navigation());
     
-    map.addControl(new OpenLayers.Control.Scale($('scale')));
-    map.addControl(new OpenLayers.Control.MousePosition({element: $('location')}));
-
-    var urbislayer = new OpenLayers.Layer.WMS(
-        "urbisFR",url_ws_urbis,
-        {layers: 'urbisFR', format: 'image/png' },
-        { tileSize: new OpenLayers.Size(256,256) }
-    );
-    map.addLayer(urbislayer);
+    var panel = new OpenLayers.Control.Panel();
+    panel.addControls([new OpenLayers.Control.ZoomIn(), new OpenLayers.Control.ZoomOut()]);
+    map.addControl(panel);
+    
+    //map.addControl(new OpenLayers.Control.Scale($('scale')));
+    //map.addControl(new OpenLayers.Control.MousePosition({element: $('location')}));
+    
+    var matrixIds = new Array(16);
+    for (var i=0; i<16; ++i) {
+        matrixIds[i] = "EPSG:31370:" + i;
+    }
+    var ortho2009 = new OpenLayers.Layer.WMTS({
+        name: "urbis ortho 2009",
+        url: "http://geoserver.gis.irisnetlab.be/gwc/service/wmts",
+        layer: "urbis:ortho2009",
+        matrixSet: "EPSG:31370",
+        matrixIds: matrixIds,
+        serverResolutions: [1112.61305859375, 556.306529296875, 278.1532646484375, 139.07663232421876, 69.53831616210938, 34.76915808105469, 17.384579040527345, 8.692289520263673, 4.346144760131836, 2.173072380065918, 1.086536190032959, 0.5432680950164795, 0.2716340475082398, 0.1358170237541199, 0.06790851187705994, 0.03395425593852997],
+        format: "image/png",
+        style: "_null",
+        opacity: 1,
+        isBaseLayer: true
+    });
+    map.addLayer(ortho2009);
     
     var icon_url = portal_url+'/++resource++cirb.novac.images/marker.png';
     var size = new OpenLayers.Size(21,25);
@@ -75,7 +90,7 @@ $(document).ready(function() {
                     }
                 });
     
-    map.setCenter(address_folder, 5);
+    map.setCenter(address_folder,11);
     
     
 });
