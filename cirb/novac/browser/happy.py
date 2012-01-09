f4e38152b095a28ec9a3fc1e292005c8f4f557f1
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-01-09 10:38:35 +0100

    add happy page and redirection homepage

---
+++ cirb/novac/browser/happy.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+from zope.interface import implements, Interface
+
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+class Happy(BrowserView):
+    """
+    happy pag browser view
+    """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request        
+    
+    @property
+    def portal_catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+
+    @property
+    def portal(self):
+        return getToolByName(self.context, 'portal_url').getPortalObject()
+    
+    def happy(self):
+        return 'happy'
