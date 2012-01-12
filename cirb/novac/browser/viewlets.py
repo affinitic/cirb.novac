e36845467bc07bade03ea601db6ea047f4a58b83
Jean-François Roche <jfroche@affinitic.be>
2012-01-12 16:13:18 +0100

    overrides default breadcrumb

--- cirb/novac/browser/viewlets.py
+++ cirb/novac/browser/viewlets.py
@@ -1,11 +1,19 @@
 # -*- coding: UTF-8 -*-
 from plone.app.layout.viewlets.common import PathBarViewlet
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile, BoundPageTemplate
+from cirb.novac.browser.novacview import NovacView
 
 
 class BreatcrumbViewlet(PathBarViewlet):
-    
+
+    def baseView(self):
+        # view attribute of this viewlet is wrong, it should be a
+        # real browser view but it is bound to the viewlet that contains this viewlet
+        # (PortalHeader being a viewlet itself).
+        return self.view.__parent__
+
     def render(self):
-        return self.index()
-    
-    index = ViewPageTemplateFile('templates/breatcrumb.pt')
+        if isinstance(self.baseView(), NovacView):
+            return BoundPageTemplate(ViewPageTemplateFile('templates/breatcrumb.pt'), self)()
+        else:
+            return self.index()
