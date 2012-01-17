dceddfc63fcf38c6d72f825f5c0dd8908379f1eb
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-01-17 09:27:19 +0100

    improve happy page

--- cirb/novac/browser/happy.py
+++ cirb/novac/browser/happy.py
@@ -17,74 +17,109 @@ class Happy(BrowserView):
     """
 
     def __init__(self, context, request):
-	self.context = context
-	self.request = request
-	self.logger = logging.getLogger('cirb.novac.happy')	
-	novac_url = os.environ.get("novac_url", None)
-	if novac_url:
-	    self.novac_url = novac_url
-	else:
-	    registry = getUtility(IRegistry)
-	    self.novac_url = registry['cirb.novac.novac_url']
+        self.context = context
+        self.request = request
+        self.logger = logging.getLogger('cirb.novac.happy')
+        novac_url = os.environ.get("novac_url", None)
+        if novac_url:
+            self.novac_url = novac_url
+        else:
+            registry = getUtility(IRegistry)
+            self.novac_url = registry['cirb.novac.novac_url']
 
     @property
     def portal_catalog(self):
-	return getToolByName(self.context, 'portal_catalog')
+        return getToolByName(self.context, 'portal_catalog')
 
     @property
     def portal(self):
-	return getToolByName(self.context, 'portal_url').getPortalObject()
+        return getToolByName(self.context, 'portal_url').getPortalObject()
 
     def happy(self):
-	results=[]
-	results.append(self.get_sso())
-	results.append(self.get_waws())
-	results.append(self.get_urbis())
-	results.append(self.access_database())
-	results.append(self.plone_version())
-	return results
+        results=[]
+        results.append(self.database_access())        
+        results.append(self.get_sso())
+        results.append(self.get_pub_waws())
+        results.append(self.get_urbis())
+        results.append(self.disk_usage())
+        return results
 
     def get_sso(self):
-	url = "https://sso.irisnet.be/"
-	results = get_service(url)
-	if results.get('status') == 'ko':
-	    return  results
-	else:
-	    return {'status':results.get('status'), 'message':'access to %s.' % url}
+        url = "https://sso.irisnet.be/"
+        results = get_service(url)
+        if results.get('status') == 'ko':
+            status = results.get('status')
+            message = "%s. Calling sso url %s failled" % (results, url)
+        else:
+            status = results.get('status')
+            message = 'Success : access to sso url (%s).' % url
+        return {"status":status, "message":message}
 
-    def get_waws(self):
-	id_dossier = '200000'
-	url = '%s/%s/%s' % (self.novac_url, PUB_DOSSIER, id_dossier)
-	results = get_service(url)
-	if results.get('status') == 'ko':
-	    return  {'status':results.get('status'),  'message':'%s. dossier : %s.' % (results.get("message"), id_dossier)}
-	else:
-	    return {'status':results.get('status'), 'message':'access to %s.' % url}
-
+    def get_pub_waws(self):
+        id_dossier = '200000'
+        url = '%s/%s/%s' % (self.novac_url, PUB_DOSSIER, id_dossier)
+        results = get_service(url)
+        if results.get('status') == 'ko':
+            status = results.get('status')
+            message = '%s. Access to public dossier %s failled.' % (results.get("message"), id_dossier)
+        else:
+            status = results.get('status')
+            message = 'Access to %s.' % url
+        return {'status':status, 'message':message}
 
     def get_urbis(self):        
-	url = "%s/gis/geoserver/nova/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=CLUSTER3KM&outputFormat=json" % self.context.portal_url()
-	res_json = get_service(url)
-	if res_json.get('status', '') == 'ko':
-	    return  {'status':res_json.get('status'),  'message':'Urbis not found. %s.' % (res_json.get("message"))}
-	resutls = json_proccessing(res_json.get("message"))
-	tot = 0
-	for feature in resutls.get('features'):
-	    tot += feature.get('properties').get('NBR_DOSSIERS')
-	return {"status":"ok", "message": "Number of dossier in Publicis %s." % tot}
+        url = "%s/gis/geoserver/nova/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=CLUSTER3KM&outputFormat=json" % self.context.portal_url()
+        res_json = get_service(url)
+        if res_json.get('status', '') == 'ko':
+            status = res_json.get('status')
+            message = '%s. Urbis not called.' % (res_json.get("message"))
+        else:
+            resutls = json_proccessing(res_json.get("message"))
+            if resutls.get("status") == "ko":
+                status = "ko"
+                message = "%s. Parsing json failled" % resutls.get("message")
+            else:
+                tot = 0
+                for feature in resutls.get('features'):
+                    tot += feature.get('properties').get('NBR_DOSSIERS')
+                status = "ok"
+                message = "Number of dossier in Publicis %s." % tot
+        return {"status":status, "message":message}
 
 
-    def access_database(self):
-	users = ['bsuttor']
-	return {"status":"ok", "message":"Access to db, number of users is %s." % len(users)}
+    def database_access(self):
+        membership = getToolByName(self, 'portal_membership')
+        try:
+            users = membership.listMembers()
+            status = "ok"
+            message = "Success : access to db, number of users is %s, Plone version is %s" % (len(users),self.plone_version() )
+        except:
+            status = "ko"
+            message = "Access to list members failled, Plone version is %s"	% self.plone_version()    
+        return {"status":status, "message":message}
 
     def plone_version(self):
-	ver = 'beta'
-	return {"status":"ok", "message":"Plone version is %s." % ver}
-    
+        migrationTool = getToolByName(self, 'portal_migration')
+        return migrationTool.getSoftwareVersion()
+
     def disk_usage(self):
-	du = 'du'
-	return {"status":"ok", "message":"Plone version is %s." % ver}	
+        s = os.statvfs('/home')
+        free = s.f_bsize * s.f_bavail
+        free_kb = free/1024
+        free_mb = free_kb/1024
+        if free_mb < 100:
+            status = "KO"
+            message = "No more space on device, avaiability %s Mb." % free_mb
+        if free_mb < 500:
+            status = "Warnings"
+            message = "Space left is wake : %s Mb" % free_mb
+        else:
+            status = "ok"
+            message = "Partition '/home' avaiability is %s Mb." %free_mb
+        return {"status":status, "message":message}
+
+
+
 
 
 def get_service(url, headers="", params=""):
@@ -92,40 +127,41 @@ def get_service(url, headers="", params=""):
     oldtimeout = socket.getdefaulttimeout()
     results = ''
     if params:
-	url = '%s?%s' % (url, params)
+        url = '%s?%s' % (url, params)
     try:
-	socket.setdefaulttimeout(7) # let's wait 7 sec        
-	request = urllib2.Request(url)
-	for header in headers:
-	    try:
-		request.add_header(header.keys()[0], header.values()[0])
-	    except:
-		logger.info('headers bad formated')
-	opener = urllib2.build_opener()
-	results = opener.open(request).read()
+        socket.setdefaulttimeout(7) # let's wait 7 sec        
+        request = urllib2.Request(url)
+        for header in headers:
+            try:
+                request.add_header(header.keys()[0], header.values()[0])
+            except:
+                logger.info('headers bad formated')
+        opener = urllib2.build_opener()
+        results = opener.open(request).read()
     except HTTPError, e:
-	exception = 'The server couldn\'t fulfill the request. URL : %s ' % url
-	logger.error(exception)
-	return {"status":'ko', "message": "%s : %s" % (e.code, e.msg)}
+        exception = 'The server couldn\'t fulfill the request. URL : %s ' % url
+        logger.error(exception)
+        return {"status":'ko', "message": "%s : %s" % (e.code, e.msg)}
     except URLError, e:
-	exception =  'We failed to reach a server. URL: %s' % url
-	logger.error(exception)
-	return {"status":'ko', "message": "%s : %s" % (e.code, e.reason)}
+        exception =  'We failed to reach a server. URL: %s' % url
+        logger.error(exception)
+        return {"status":'ko', "message": "%s : %s" % (e.code, e.reason)}
 
     finally:
-	socket.setdefaulttimeout(oldtimeout)
+        socket.setdefaulttimeout(oldtimeout)
     return {'status':'ok', 'message': results}
+
 
 def json_proccessing(res_json):
     logger = logging.getLogger('cirb.novac.happy')
     try:
-	jsondata = json.loads(res_json)
+        jsondata = json.loads(res_json)
     except ValueError, e:
-	msg_error = 'Json value error : %s.' % e.message
-	logger.error(msg_error)
-	return {"status":'ko', "message": msg_error}
+        msg_error = 'Json value error : %s.' % e.message
+        logger.error(msg_error)
+        return {"status":'ko', "message": msg_error}
     except SyntaxError, e:
-	msg_error = 'Json bad formatted : %s.' % e.message
-	logger.error(msg_error)
-	return {"status":'ko', "message": msg_error}
+        msg_error = 'Json bad formatted : %s.' % e.message
+        logger.error(msg_error)
+        return {"status":'ko', "message": msg_error}
     return jsondata
