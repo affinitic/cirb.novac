5deb0868fa773dbe351e9a3d0015309ee59c987c
Catherine Corbesier <ccorbesier@cirb.irisnet.be>
2014-04-14 14:17:05 +0200

    Use bootstrap's tooltip

--- cirb/novac/browser/scripts/generic_methods.js
+++ cirb/novac/browser/scripts/generic_methods.js
@@ -1,8 +1,9 @@
 function tooltip_init(){
     $(".infotooltip img[title]").tooltip({
-        position: "center right",
-        offset: [-2, 10],
-        opacity: 0.9
-
+        //position: "center right",
+        //offset: [-2, 10],
+        //opacity: 0.9
+        placement:'right',
+        html:'true'
     });
 }
